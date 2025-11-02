341ac4481f72517a2544d718e1239b449e451d4d
ShreyaSomvanshi <shreyamisthi@gmail.com>
2025-11-02 19:58:41 +0530

    change email service

--- backend/config/nodemailer.js
+++ backend/config/nodemailer.js
@@ -1,10 +1,11 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host:smtp-relay.brevo.com,
+    port:587,
     auth:{
-        user:process.env.SENDER_EMAIL,
-        pass:process.env.EMAIL_APP_PASS
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PAS
     }
 })
 
