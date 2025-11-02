4ee842726f6e3adbbba9239dd6315b8213408c54
ShreyaSomvanshi <shreyamisthi@gmail.com>
2025-11-03 00:02:46 +0530

    switch to sendgrid

--- backend/config/nodemailer.js
+++ backend/config/nodemailer.js
@@ -1,13 +1,44 @@
-import nodemailer from "nodemailer";
+// import nodemailer from "nodemailer";
 
-const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth:{
-        user: process.env.SENDER_EMAIL,
-        pass: process.env.EMAIL_APP_PASSWORD
+// const transporter = nodemailer.createTransport({
+//     host: "smtp.gmail.com",
+//     port: 465,
+//     secure: true,
+//     auth:{
+//         user: process.env.SENDER_EMAIL,
+//         pass: process.env.EMAIL_APP_PASSWORD
+//     }
+// })
+
+// export default transporter;
+
+import sgMail from '@sendgrid/mail';
+
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+
+const sendMail = async (mailOptions) => {
+   
+    const msg = {
+        to: mailOptions.to,
+        from: process.env.SENDER_EMAIL, 
+        subject: mailOptions.subject,
+        text: mailOptions.text,
+
+    };
+
+    try {
+        await sgMail.send(msg);
+        console.log('Email sent successfully via SendGrid');
+    } catch (error) {
+        console.error('Failed to send email via SendGrid:', error.response ? error.response.body : error);
+        throw new Error('Failed to send email.');
     }
-})
+};
+
+const transporter = {
+    sendMail: sendMail
+};
 
 export default transporter;
