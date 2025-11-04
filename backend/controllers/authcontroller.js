47f7c2c2045f6cea08ce8cb87beb5ae590582700
ShreyaSomvanshi <shreyamisthi@gmail.com>
2025-11-04 21:26:46 +0530

    simplify cookie

--- backend/controllers/authcontroller.js
+++ backend/controllers/authcontroller.js
@@ -42,9 +42,8 @@ export const register = async (req,res) => {
 
         res.cookie("token",token,{
             httpOnly:true,
-            secure:process.env.NODE_ENV === "production",
-            sameSite:process.env.NODE_ENV === "production"?
-            "none": "strict",
+            secure:true,
+            sameSite:none,
             maxAge:7*24*60*60*1000
         });
         
@@ -81,9 +80,8 @@ export const login = async (req,res) => {
         const token = jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:"7d"});
         res.cookie("token",token,{
             httpOnly:true,
-            secure:process.env.NODE_ENV === "production",
-            sameSite:process.env.NODE_ENV === "production"?
-            "none": "strict",
+            secure:true,
+            sameSite:"None",
             maxAge:7*24*60*60*1000
         });
         return res.json({success:true});
