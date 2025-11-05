139553dd89eb070c0f39e3304954f803f3eab1ee
ShreyaSomvanshi <shreyamisthi@gmail.com>
2025-11-05 15:28:06 +0530

    commit change

--- backend/controllers/authcontroller.js
+++ backend/controllers/authcontroller.js
@@ -42,8 +42,8 @@ export const register = async (req,res) => {
 
         res.cookie("token",token,{
             httpOnly:true,
-            secure:true,
-            sameSite:"None",
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production" ? "None":"Strict",
             maxAge:7*24*60*60*1000
         });
         
@@ -80,8 +80,8 @@ export const login = async (req,res) => {
         const token = jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:"7d"});
         res.cookie("token",token,{
             httpOnly:true,
-            secure:true,
-            sameSite:"None",
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production" ? "None":"Strict",
             maxAge:7*24*60*60*1000
         });
         return res.json({success:true});
@@ -94,8 +94,8 @@ export const logout = async (req,res) => {
     try{
         res.clearCookie("token",{
             httpOnly:true,
-            secure:true,
-            sameSite:"None",
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production" ? "None":"Strict",
         })
         return res.json({success:true, message:"Logged Out"});
     }
