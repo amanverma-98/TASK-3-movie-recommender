2825afa70a25a24bb471020d32d48e7c26914908
ShreyaSomvanshi <shreyamisthi@gmail.com>
2025-11-05 15:03:08 +0530

    commit cookie change

--- backend/controllers/authcontroller.js
+++ backend/controllers/authcontroller.js
@@ -94,8 +94,8 @@ export const logout = async (req,res) => {
     try{
         res.clearCookie("token",{
             httpOnly:true,
-            secure:process.env.NODE_ENV === "production",
-            sameSite:process.env.NODE_ENV === "production"?"none": "strict",
+            secure:true,
+            sameSite:"None",
         })
         return res.json({success:true, message:"Logged Out"});
     }
